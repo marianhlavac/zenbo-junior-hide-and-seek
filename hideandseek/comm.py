62be1b6777f13a76e7e919fb9d85aed67a3e0c07
Marián Hlaváč <hlavac95@gmail.com>
2019-12-20 12:23:01 +0800

    Fix on state change handler

--- hideandseek/comm.py
+++ hideandseek/comm.py
@@ -1,8 +1,9 @@
 import pyzenbo
 
-def _on_state_change_callback(serial, cmd, error_code, state):
-    print('State change: serial: {}, cmd: {}, error_code: {}, state: {}'.format(serial, cmd, error_code, state))
+def on_state_change(serial, cmd, error, state):
+    msg = 'on_state_change serial:{}, cmd:{}, error:{}, state:{}'
+    print(msg.format(serial, cmd, error, state))
 
 def connect_robot(ip) -> pyzenbo.PyZenbo:
     print('Connecting to the robot ({})...'.format(ip))
-    return pyzenbo.connect(ip, _on_state_change_callback)
+    return pyzenbo.connect(ip, on_state_change)
